21669e6b6ef2c8050570ac8520bf308801d91a03
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-06-05 08:31:14 +1000

    added repository

---
+++ src/ToDoAPI/Data/Repositories/IToDosRepository.cs
@@ -0,0 +1,18 @@
+﻿using ToDoAPI.Models;
+
+namespace ToDoAPI.Data.Repositories
+{
+    public interface IToDosRepository
+    {
+        Task<IEnumerable<ToDoItem>> GetAll();
+        Task<ToDoItem?> GetById(int id);
+
+        Task Add(ToDoItem toDoItem);
+
+        Task Update(int id, ToDoItem toDoItem);
+
+        Task Delete(int id);
+
+        Task SaveChanges();
+    }
+}
---
+++ src/ToDoAPI/Data/Repositories/ToDosRepository.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoAPI.Models;
+
+namespace ToDoAPI.Data.Repositories
+{
+    public class ToDosRepository : IToDosRepository
+    {
+        private AppDbContext _appDbContext;
+
+        public ToDosRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<IEnumerable<ToDoItem>> GetAll()
+        {
+            return await _appDbContext.ToDoItems.ToListAsync();
+        }
+
+        public async Task<ToDoItem?> GetById(int id)
+        {
+            return await _appDbContext.ToDoItems.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task Add(ToDoItem toDoItem)
+        {           
+            await _appDbContext.ToDoItems.AddAsync(toDoItem);
+            await SaveChanges();
+        }
+
+        public async Task Update(int id, ToDoItem toDoItem)
+        {
+            var toDoFromDB = await _appDbContext.ToDoItems.FirstOrDefaultAsync(x => x.Id == id);
+            if (toDoFromDB == null)
+            {
+                throw new ArgumentException($"No ToDoItem with such ID {id}");
+            }
+            toDoFromDB.Name = toDoItem.Name;
+            await SaveChanges();
+        }
+
+        public async Task Delete(int id)
+        {
+            var toDoFromDB = await _appDbContext.ToDoItems.FirstOrDefaultAsync(x => x.Id == id);
+            if (toDoFromDB == null)
+            {
+                throw new ArgumentException($"No ToDoItem with such ID {id}");
+            }
+            _appDbContext.ToDoItems.Remove(toDoFromDB);
+            await SaveChanges();
+        }
+
+        public async Task SaveChanges()
+        {
+            await _appDbContext.SaveChangesAsync ();
+        }
+    }
+}
