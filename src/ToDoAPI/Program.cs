6ab470339487922d271d08c575e02c45c69ed052
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-06-05 08:40:34 +1000

    added end points to program.cs

--- src/ToDoAPI/Program.cs
+++ src/ToDoAPI/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using ToDoAPI.Data;
+using ToDoAPI.Data.Repositories;
+using ToDoAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,5 +23,51 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// API end points
+// get multiple
+app.MapGet("api/v1/todo", async (IToDosRepository repo) => {
+    return Results.Ok(await repo.GetAll());
+});
+
+// get single
+app.MapGet("api/v1/todo/{id}", async (IToDosRepository repo, int id) => {
+    var todo = await repo.GetById(id);
+    if (todo == null)
+        return Results.NotFound($"No such todo with {id}");
+    return Results.Ok(todo);
+});
+
+// create
+app.MapPost("api/v1/todo", async (IToDosRepository repo, ToDoItem todo) => {
+    await repo.Add(todo);
+    await repo.SaveChanges();
+    // rest conventions - route to access new command and new object
+    return Results.Created($"api/v1/todo/{todo.Id}", todo);
+});
+
+// update
+app.MapPut("api/v1/todo/{id}", async (IToDosRepository repo, int id, ToDoItem todo) => {
+    var commandFromDb = await repo.GetById(id);
+    if (commandFromDb == null)
+        return Results.NotFound($"No such command with {id}");
+
+    // update and save changes
+    await repo.Update(id, todo);
+
+    // rest recomendation for out
+    return Results.NoContent();
+});
+
+// delete
+app.MapDelete("api/v1/todo", async (IToDosRepository repo, int id) => {
+    var toDoToDelete = await repo.GetById(id);
+    if (toDoToDelete == null)
+        return Results.NotFound($"No such todo with {id}");
+
+    await repo.Delete(id);
+
+    // return resource which was deleted
+    return Results.Ok(toDoToDelete);
+});
 
 app.Run();
